eb80ab82b7d551484e471da834746eda81f46a0a
pjmantoss <joelptoss@gmail.com>
2020-11-10 15:09:24 +0100

    update script

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ async function getPhotos(){
 }
 
 function getMyPhotosHtml(photos){
-    let myPhotosHtml = photos.map().join();
+    let myPhotosHtml = photos.map(photo => {
+        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}">`
+    }).join(
+        return `<img class="my-photos">`
+    );
 }
