12099073723619bd3db9a2f24a20843998bee4a9
pjmantoss <joelptoss@gmail.com>
2020-11-10 15:10:58 +0100

    update script

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ async function getPhotos(){
 function getMyPhotosHtml(photos){
     let myPhotosHtml = photos.map(photo => {
         return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}">`
-    }).join(
-        return `<img class="my-photos">`
-    );
+    }).join('');
+    return `<div class="my-photos">${myPhotosHtml}</div>`
 }
